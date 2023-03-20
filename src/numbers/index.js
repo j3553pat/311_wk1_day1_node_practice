2699c67d14d81af0662ed1433ae2aedb1ae1b861
j3553pat <jessecodedev@gmail.com>
2023-03-20 17:16:05 -0500

    complete

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -9,16 +9,23 @@ console.log(isEven(2))
 
 const sum = (arr) => {
   // write code for numbers.sum
-  for(let i = 0; i < arr.length; i++){
-     arr.reduce()
-  }
-  return arr
+  return arr.reduce((x, y) => x + y)
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  for(let i = 0; i < arr.length; i ++) {
+    for(let j = 0; j < arr.length; j++) {
+      if(arr[i] + arr[j] == sum) {
+        return true
+      }
+      else {
+        return false
+      }
+    }
+  }
 }
+
 
 module.exports = {
   isEven,
--- src/strings/index.js
+++ src/strings/index.js
@@ -4,8 +4,15 @@ const split = (str, delim) => {
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  let final;
+  let arr = [];
+  for(let i = 0; i <= str.length - 1; i += 2) {
+    final = str.substr(i, 2)
+    arr.push(final)
+  }
+  return arr
 }
+
 
 const reverse = (str) => {
   // write code for strings.reverse
